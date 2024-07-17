abc67ba26b0b38d84b3d499a9fae315806e336a7
Mohammed-AlTuaijri <ma.altuaijri@gmail.com>
2024-07-17 11:38:59 +0300

    test2

--- app/common/Books/BooksCRUD.py
+++ app/common/Books/BooksCRUD.py
@@ -1,38 +1,11 @@
 from sqlalchemy.orm import Session
-
 from . import models
 from . import schemas
-
-import uuid
-# def get_author(db: Session, author_id: int):
-#     return db.query(models.Author).filter(models.Author.id == author_id).first()
-
-
-# def get_author_by_first_name(db: Session, first_name: str):
-#     return db.query(models.Author).filter(models.Author.first_name == first_name).first()
-
-
-# def get_authors(db: Session, skip: int = 0, limit: int = 100):
-#     return db.query(models.Author).offset(skip).limit(limit).all()
-
-
-# def create_book(db: Session, book: schemas.BookCreate):
-#     # fake_hashed_password = user.password + "notreallyhashed"
-#     db_book = models.Book(title= book.title, description = book.description, id=book.id, author_id = book.author_id)
-#     db.add(db_book)
-#     db.commit()
-#     db.refresh(db_book)
-#     return db_book
-
 
 def get_books(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Book).offset(skip).limit(limit).all()
 
-
-
-def create_book(db: Session, book: schemas.Books):
-    # uid = uuid.uuid4()
-    # book_id = str(uid)
+def create_book(db: Session, book: schemas.BooksCreate):
     db_book = models.Book(
                            title=book.title, 
                            genre = book.genre, 
@@ -42,3 +15,23 @@ def create_book(db: Session, book: schemas.Books):
     db.commit()
     db.refresh(db_book)
     return db_book
+
+def get_single_book(db: Session, id):
+    return db.query(models.Book).filter(models.Book.book_id == id).first()
+
+def update_book(db: Session, book: schemas.BooksCreate, id):
+    book_to_update = db.query(models.Book).filter(models.Book.book_id == id).first()
+    book_to_update.title = book.title
+    book_to_update.genre = book.genre
+    book_to_update.description = book.description
+    book_to_update.Author_id = book.Author_id
+    db.commit()
+    db.refresh(book_to_update)
+    return book_to_update
+
+
+def delete_book(db: Session, id):
+    book_to_delete = db.query(models.Book).filter(models.Book.book_id == id).first()
+    db.delete(book_to_delete)
+    db.commit()
+    return book_to_delete
--- app/common/Books/BooksRouter.py
+++ app/common/Books/BooksRouter.py
@@ -1,60 +1,32 @@
-# from fastapi import Form
-# from fastapi import APIRouter, Response, Depends
-# from sqlalchemy.orm import Session
-# from common.schema import schemas
-# from typing import List, Union
-# app = APIRouter()
-# from . import BooksCRUD
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from . import schemas
+from app.common.database import get_db
+from . import BooksCRUD
 
+app = APIRouter()
 
-# db2 = [{'id': 1, 'name': "test", 'author': "John Doe", 'desc': "philosophical dude"},
-#        {'id': 2, 'name': "test2", 'author': "Jane Doe", 'desc': "thriller writer from 1801"},
-#     ]
-# db: List[Books] = [
-#     Books(
-#         id= 1,
-#         name="test",
-#         author="john doe",
-#         desc="philosophical dude"
-#     ),
-#     Books(
-#         id= 2,
-#         name="test2",
-#         author="jane doe",
-#         desc="thriller writer from 1801"
-#     )
-# ]
+# GET /books: Retrieve a list of all books.
+@app.get("/books/", response_model=list[schemas.Books], tags=["books"])
+def retrieve_all_books(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return BooksCRUD.get_books(db, skip=skip, limit=limit)
 
-# # GET /books: Retrieve a list of all books.
-# @app.get("/books",tags=["books"]) 
-# async def retrieve_books():
-#   # Call a Function in services and return a list
-#   return BooksCRUD.get_books()
-
-# @app.get("/books/", response_model=list[schemas.Books])
-# def retrieve_books(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-#     books = BooksCRUD.get_books(db, skip=skip, limit=limit)
-#     return books
+# POST /books: Create a new book record (Admin only).
+@app.post("/books", tags=["books"]) 
+async def create_book(book: schemas.BooksCreate, db: Session = Depends(get_db)):
+  return BooksCRUD.create_book(db, book)
 
 # GET /books/:id: Retrieve details of a specific book by its ID.
-# @app.get("/books/{id}", tags=["books"]) 
-# async def retrieve_book(id: int):
-#   # Call Function in services and return details
-#     for book in db2:
-#        if book["id"] == id:
-#           return book
-#     return {"None Found"}
-
-#POST /books: Create a new book record (Admin only).
-# @app.post("/books",response_model = Books, tags=["books"]) 
-# async def create_book(book: Books):
-#   # Call a Function in services to add a new book
-  
-#   return BooksCRUD.create_book(book)
+@app.get("/books/{id}", response_model=schemas.Books, tags=["books"])
+async def retrieve_single_book(id: int, db: Session = Depends(get_db)):
+    return BooksCRUD.get_single_book(db, id)
 
 # PUT /books/:id: Update an existing book record by its ID (Admin only).
-# @app.post("/books/id",tags=["books"]) 
-# async def update_book():
-#   # Call a Function in services to update a book
+@app.put("/books/{id}", response_model=schemas.BooksCreate, tags=["books"]) 
+async def update_book(id: int, book: schemas.BooksCreate, db: Session = Depends(get_db)):
+  return BooksCRUD.update_book(db, book, id)
 
-#   return test()
+# DELETE /books/:id: Delete a book record by its ID (Admin only).
+@app.delete("/books/{id}", response_model=schemas.BooksCreate, tags=["books"])
+async def delete_book(id: int , db: Session = Depends(get_db)):
+   return BooksCRUD.delete_book(db, id)
