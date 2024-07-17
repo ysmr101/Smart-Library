a43e8557998573aec28aec9fb0b8a40f49e97244
anoudalysf <anoud-s-alyousef@hotmail.com>
2024-07-17 14:49:31 +0300

    Update AuthorsRouter.py

--- app/common/authors/AuthorsRouter.py
+++ app/common/authors/AuthorsRouter.py
@@ -9,14 +9,14 @@ app = APIRouter()
 
 # GET all authors
 @app.get("/authors", response_model=list[AuthorsSchema.Author], tags=["author"])
-def retrieve_authors(db: Session = Depends(get_db), skip: int = 0, limit: int = 100):
+def retrieve_all_authors(db: Session = Depends(get_db), skip: int = 0, limit: int = 100):
     authors = AuthorsCRUD.read_all_authors(db, skip=skip, limit=limit)
     return authors
 
 
 # GET an author by ID
 @app.get("/authors/{author_id}", response_model=AuthorsSchema.Author, tags=["author"])
-async def read_author(author_id: int, db: Session = Depends(get_db)):
+async def retrieve_specific_authors(author_id: int, db: Session = Depends(get_db)):
     db_author = AuthorsCRUD.read_author(db, author_id=author_id)
     # if db_author is None:
     #     raise HTTPException(status_code=404, detail="Author not found")
@@ -25,7 +25,7 @@ async def read_author(author_id: int, db: Session = Depends(get_db)):
 
 # POST a new author
 @app.post("/authors/", response_model=AuthorsSchema.Author, tags=["author"])
-def create_author(author: AuthorsSchema.AuthorAdd, db: Session = Depends(get_db)):
+def add_new_author(author: AuthorsSchema.AuthorAdd, db: Session = Depends(get_db)):
     return AuthorsCRUD.create_author(db, author=author)
 
 
