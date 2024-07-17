3613f883640a9c56881e5fa445b62a98a2fc730b
Yasser AlRuwaiti <Yasser.alruwaiti@pwc.com>
2024-07-17 22:17:10 +0300

    added update user

--- app/common/user/UserCRUD.py
+++ app/common/user/UserCRUD.py
@@ -1,21 +1,39 @@
 from sqlalchemy.orm import Session
+from fastapi import HTTPException
 import uuid
 from app.common.utils import auth
 from app.common.user import UserModel, UserSchema
+
 model = UserModel
 schema = UserSchema.UserCreate
 
-def get_user_byId(db: Session, user_id: int):
+
+def get_user_byId(db: Session, user_id: str):
     return db.query(model.User).filter(model.User.user_id == user_id).first()
+
 
 def get_user_by_username(db: Session, user_name: str):
     return db.query(model.User).filter(model.User.username == user_name).first()
 
 
-
-
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(model.User).offset(skip).limit(limit).all()
+
+
+def update_user(db: Session, user: schema, user_id: str):
+    print("hereeeeeeeeeeeee")
+    db_user = get_user_byId(db, user_id)
+    print(db_user)
+    if db_user == None:
+        raise HTTPException(status_code=400, detail="Bad Request")
+    hashed_password = auth.get_password_hash(user.password)
+    db_user.username = user.user_name
+    db_user.password_hash = hashed_password
+
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
 
 
 def create_user(db: Session, user: schema):
--- app/common/user/UserRouter.py
+++ app/common/user/UserRouter.py
@@ -5,19 +5,23 @@ from app.common.user import UserCRUD, UserModel
 from app.common.config.database import get_db
 from typing import Annotated
 from app.common.utils import auth
+
 app = APIRouter()
+
+
 @app.post("/users/register", tags=["users"])
 def create_user(user: UserCreate, db: UserCRUD.Session = Depends(get_db)):
-    print(user.user_name)
     db_user = UserCRUD.get_user_by_username(db, user.user_name)
-    print(str(db_user))
     if db_user:
         raise HTTPException(status_code=400, detail="Username is already registered")
     return UserCRUD.create_user(db, user)
+
+
 @app.post("/login", tags=["users"])
-async def login_for_access_token(form_data: Annotated[auth.OAuth2PasswordRequestForm, Depends()], db: UserCRUD.Session = Depends(get_db) ) -> auth.Token:
+async def login_for_access_token(form_data: Annotated[auth.OAuth2PasswordRequestForm, Depends()],
+                                 db: UserCRUD.Session = Depends(get_db)) -> auth.Token:
     users = UserCRUD.get_users(db, 0, 100)
-    print(form_data.username,form_data.password)
+    print(form_data.username, form_data.password)
     print(users[0])
     user = auth.authenticate_user(db, form_data.username, form_data.password)
     if not user:
@@ -31,8 +35,15 @@ async def login_for_access_token(form_data: Annotated[auth.OAuth2PasswordRequest
         data={"sub": user.username}, expires_delta=access_token_expires
     )
     return auth.Token(access_token=access_token, token_type="bearer")
+
+
 @app.get("/users/me/", tags=["users"])
 async def read_users_me(
-    current_user: Annotated[UserModel.User, Depends(auth.get_current_user)],
+        current_user: Annotated[UserModel.User, Depends(auth.get_current_user)],
 ):
     return current_user
+
+
+@app.put("/users/me/{user_id}", tags=["users"])
+async def update_users_me(user_id: str, user: UserCreate, db: UserCRUD.Session = Depends(get_db)):
+    return UserCRUD.update_user(db, user, user_id)
