9df12b0f8eeb47952041829feff8c4ff5bec5619
Yasser AlRuwaiti <Yasser.alruwaiti@pwc.com>
2024-07-18 09:57:57 +0300

    Changing some of the login logic

--- app/common/utils/auth.py
+++ app/common/utils/auth.py
@@ -50,6 +50,21 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     encoded_jwt = jwt.api_jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
+#Should probably be a service
+def access_token(db: Session, username: str,  password: str):
+    user = authenticate_user(db, username, password)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: Session = Depends(get_db)):
     credentials_exception = HTTPException(
