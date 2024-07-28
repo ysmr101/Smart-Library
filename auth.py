cbceae485d2060799c5e28f0cdf29ad1ebc68012
Mohammed-AlTuaijri <ma.altuaijri@gmail.com>
2024-07-28 09:59:43 +0300

    Add files via upload

---
+++ auth.py
@@ -0,0 +1,131 @@
+from datetime import datetime, timedelta, timezone
+from typing import Annotated
+
+
+from sqlalchemy.orm import Session
+import jwt
+from fastapi import (
+    Depends,
+    HTTPException,
+    status,
+    APIRouter,
+    Response,
+    Security,
+    security,
+)
+from fastapi.security import (
+    OAuth2PasswordBearer,
+    OAuth2PasswordRequestForm,
+    HTTPAuthorizationCredentials,
+    HTTPBearer,
+)
+from jwt.exceptions import InvalidTokenError
+from passlib.context import CryptContext
+from pydantic import BaseModel
+from app.user import user_model, user_crud
+from app.common.config.database import get_db
+
+
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+security = HTTPBearer()
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/login")
+app = APIRouter()
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def authenticate_user(db: Session, username: str, password: str):
+    user = user_crud.get_user_by_username(db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.password_hash):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.api_jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+# Should probably be a service
+def access_token(response: Response, db: Session, username: str, password: str):
+    user = authenticate_user(db, username, password)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    response.set_cookie(
+        key="access_token", value=f"Bearer {access_token}", httponly=True
+    )
+    return Token(access_token=access_token, token_type="bearer")
+
+
+class JWTError(BaseException):
+    pass
+
+
+def get_current_user(
+    token: HTTPAuthorizationCredentials = Security(security),
+    db: Session = Depends(get_db),
+):
+    try:
+        payload = jwt.decode(token.credentials, SECRET_KEY, algorithms=[ALGORITHM])
+        name: str = payload.get("sub")
+        if name is None:
+            raise HTTPException(
+                status_code=401, detail="Invalid authentication credentials"
+            )
+        user = user_crud.get_user_by_username(db, name)
+        if user is None:
+            raise HTTPException(status_code=401, detail="User not found")
+        return user
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+
+class RoleChecker(BaseException):
+    def __init__(self, allowed_roles):
+        self.allowed_roles = allowed_roles
+
+    def __call__(self, user: Annotated[user_model.User, Depends(get_current_user)]):
+        if user.role in self.allowed_roles:
+            return True
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="You don't have enough permissions",
+        )
---
+++ books_schema.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel
+
+
+class Books(BaseModel):
+    book_id: int
+    title: str
+    genre: str
+    description: str
+    author_id: int
+
+
+class Books_create(BaseModel):
+    title: str
+    genre: str
+    description: str
+    author_id: int
+
+
+class User_preferences(BaseModel):
+    preference_id: int
+    user_id: str
+    preferences: str
+
+
+class User_preferences_create(BaseModel):
+    # user_id: str
+    preferences: str
+
+
+class QueryModel(BaseModel):
+    query: str
