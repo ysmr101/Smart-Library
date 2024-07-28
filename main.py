f8a84bdfdfd6a79d736c4663595cf9e14c287df2
Yasser AlRuwaiti <Yasser.alruwaiti@pwc.com>
2024-07-28 16:00:25 +0300

    frontend task 2

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ from app.user import user_router
 from app.authors import authors_router
 from app.Books import books_router
 from app.common.config.database import engine, Base
-
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:63342"],  # List of allowed origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 app.include_router(user_router.app)
 app.include_router(authors_router.app)
