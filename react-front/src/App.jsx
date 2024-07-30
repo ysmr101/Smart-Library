8284046c80df4561c169c9d5c253d207edabb2fd
Yasser AlRuwaiti <Yasser.alruwaiti@pwc.com>
2024-07-30 10:21:20 +0300

    frontend task 3 - added react-spinners

--- react-front/src/App.jsx
+++ react-front/src/App.jsx
@@ -1,16 +1,24 @@
 import React, { useState, useEffect } from 'react';
+import ClipLoader from 'react-spinners/ClipLoader';
 import './App.css';
 import Search from './components/Search';
 import BookCard from './components/BookCard';
 
 function App() {
   const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch('http://127.0.0.1:8000/books/?start=0&limit=100')
       .then(response => response.json())
-      .then(data => setBooks(data))
-      .catch(error => console.error('Error loading the books:', error));
+      .then(data => {
+        setBooks(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error loading the books:', error);
+        setLoading(false);
+      });
   }, []);
 
   return (
@@ -21,13 +29,19 @@ function App() {
       <div className="container">
         <Search />
       </div>
-      <div className="row" id="book-list">
-        {books.map(book => (
-          <div className="column" key={book.id}>
-            <BookCard thumbnail={book.thumbnail} title={book.title} />
-          </div>
-        ))}
-      </div>
+      {loading ? (
+        <div className="spinner">
+          <ClipLoader size={100} color={"#090000"} loading={loading} />
+        </div>
+      ) : (
+        <div className="row" id="book-list">
+          {books.map(book => (
+            <div className="column" key={book.id}>
+              <BookCard thumbnail={book.thumbnail} title={book.title} />
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
